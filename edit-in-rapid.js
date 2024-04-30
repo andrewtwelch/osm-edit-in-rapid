7ec2ec30d4eeb6726a2a8420312a5c6d277079fe
Andrew Welch <10568504+andrewtwelch@users.noreply.github.com>
2024-04-30 23:38:47 +0930

    Add additional metadata

--- edit-in-rapid.js
+++ edit-in-rapid.js
@@ -1,8 +1,11 @@
 // ==UserScript==
 // @name         Add Rapid editor to OpenStreetMap
-// @namespace    http://tampermonkey.net/
 // @version      0.0.1
 // @description  Adds the Rapid editor as an option for editing OpenStreetMap
+// @license      GPL-3.0-or-later; https://www.gnu.org/licenses/gpl-3.0.txt
+// @namespace    https://github.com/andrewtwelch/osm-edit-in-rapid
+// @downloadURL  https://raw.github.com/andrewtwelch/osm-edit-in-rapid/main/edit-in-rapid.js
+// @updateURL    https://raw.github.com/andrewtwelch/osm-edit-in-rapid/main/edit-in-rapid.js
 // @author       Andrew Welch
 // @match        https://www.openstreetmap.org/**
 // @grant        none
